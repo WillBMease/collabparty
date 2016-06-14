328ae24570da71d8a1104584e8d3c85ba840ecdd
WillBMease <will@jellyvibes.com>
2016-06-14 15:07:30 -0700

    d

--- assets/js/youtube.js
+++ assets/js/youtube.js
@@ -8,9 +8,9 @@ $('#ytsearch').each(function() {
         elem.data('oldVal', elem.val());
 
     		var request = gapi.client.youtube.search.list({
-    			// part: "snippet",
+    			part: "snippet",
     			// type: "video",
-          type: "search",
+          // type: "search",
     			q: encodeURIComponent(elem.val()).replace(/%20/g, '+'),
     			maxResults: 10,
     			order: "viewCount"
