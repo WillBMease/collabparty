39d05099bd40ccac01f41a1a6d7e9bbfb82b591b
WillBMease <will@jellyvibes.com>
2016-09-10 02:40:04 -0700

    d

--- assets/js/main.js
+++ assets/js/main.js
@@ -21,7 +21,7 @@ function ping(){
 	}
 }
 
-player = document.getElementsById('song')
+player = document.getElementById('song')
 var audioControl = context.createMediaElementSource(player)
 var volume = context.createGain()
 audioControl.connect(volume)
