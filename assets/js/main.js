c652893d82dee778a52ec27c63cf7cb0d057a8a5
WillBMease <will@jellyvibes.com>
2016-09-09 19:22:07 -0700

    D

--- assets/js/main.js
+++ assets/js/main.js
@@ -12,16 +12,12 @@ var checkLoadedInt, currentSong = null, songs = null
 var code = null, myid = Math.floor(Math.random() * 99999)
 var mobileReady = true, scrubber = false
 
-// var playlist = function(){
-//
-// }
-
-$('#enableContainer').click(function(){
-  $('#song').get(0).play()
-  $('#song').get(0).pause()
-  $('#enableContainer').remove()
-  mobileReady = true
-})
+// $('#enableContainer').click(function(){
+//   $('#song').get(0).play()
+//   $('#song').get(0).pause()
+//   $('#enableContainer').remove()
+//   mobileReady = true
+// })
 
 function detectmob() {
  if( navigator.userAgent.match(/Android/i)
@@ -77,16 +73,16 @@ function ping(){
 
 var open = false
 
-$('#invite').click(function(){
-	if (!open){
-		$('.sidebar').css('left', '0px')
-		open = true
-	}
-	else {
-		$('.sidebar').css('left', '-200px')
-		open = false
-	}
-})
+// $('#invite').click(function(){
+// 	if (!open){
+// 		$('.sidebar').css('left', '0px')
+// 		open = true
+// 	}
+// 	else {
+// 		$('.sidebar').css('left', '-200px')
+// 		open = false
+// 	}
+// })
 
 player = document.getElementById('song')
 var audioControl = context.createMediaElementSource(player)
@@ -131,11 +127,7 @@ function addSongToList(data){
   songs.push(data)
 
   $('#song'+data.videoid).click(function(){
-    console.log('song click')
-    console.log(currentSong.videoid)
-    console.log(data.videoid)
     if (currentSong.videoid != data.videoid){
-      console.log('sending for change song')
       io.socket.post('/Room/changeSong', {videoid: data.videoid, id: myid, code: code})
     }
     else {
@@ -172,9 +164,9 @@ function loadSong(url){
 
 var updateTimeInt, player, myAudio, playing = false
 
-$('#play').click(function(){
-  clickPlay()
-})
+// $('#play').click(function(){
+//   clickPlay()
+// })
 
 function clickPlay(){
   if (!playing){
@@ -224,12 +216,12 @@ var Learn = function(){
     if (status == 'above'){
       if (this.increment > 0)
         this.increment = 0
-      this.increment -= 0.0012
+      this.increment -= 0.0010
     }
     else if (status == 'below'){
       if (this.increment < 0)
         this.increment = 0
-      this.increment += 0.0012
+      this.increment += 0.0010
     }
     return this.increment
   }
@@ -260,131 +252,115 @@ function updateScrubber(){
   $('.playerTime').text('- ' + mins + ':' + secs)
 }
 
-// var App = function() {
-//   connect: function(){
-//     code = (window.location.hash).replace('#', '')
-//   	$('.uuid').text(code)
-//     io.socket.post('/Room/join', {code: code}, function(data){
-//       songs = data.songs
-//       $('.song-list').empty()
-//       songs.forEach(function (s, i){
-//         addSongToList(s)
-//       })
-//       if (data.currentSong){
-//         songs.forEach(function (s, i){
-//           if (s.videoid == data.currentSong)
-//             changeSong(s)
-//         })
-//       }
+var Test = {
+  test: function(){
+    alert('worked')
+  }
+}
+
+var test = new Test()
+test.test()
+
+
+// io.socket.on('connect', function(){
+//   code = (window.location.hash).replace('#', '')
+// 	$('.uuid').text(code)
+//   io.socket.post('/Room/join', {code: code}, function(data){
+//     songs = data.songs
+//     $('.song-list').empty()
+//     songs.forEach(function (s, i){
+//       addSongToList(s)
 //     })
-//   }
-// }
+//     if (data.currentSong){
+//       songs.forEach(function (s, i){
+//         if (s.videoid == data.currentSong)
+//           changeSong(s)
+//       })
+//     }
+//   })
+//   // app.connect()
+// })
 //
-// var app = new App()
-
-
-io.socket.on('connect', function(){
-  code = (window.location.hash).replace('#', '')
-	$('.uuid').text(code)
-  io.socket.post('/Room/join', {code: code}, function(data){
-    songs = data.songs
-    $('.song-list').empty()
-    songs.forEach(function (s, i){
-      addSongToList(s)
-    })
-    if (data.currentSong){
-      songs.forEach(function (s, i){
-        if (s.videoid == data.currentSong)
-          changeSong(s)
-      })
-    }
-  })
-  // app.connect()
-})
-
-
-io.socket.on('play', function (data){
-  if (myid != data.id){
-    var offset = parseFloat(low.offset) - parseFloat(data.offset)
-    var delay = ((+new Date() - data.time + offset) / 1000).toFixed(6)
-    if (delay < 0)
-      delay = 0
-
-    player.currentTime = parseFloat( (player.currentTime).toFixed(6) + parseFloat(delay) )
-    play()
-  }
-})
-
-io.socket.on('pause', function (data){
-  $('#play').css('background-image', 'url(/images/play.jpg)')
-  if (myid != data.id){
-    player.pause()
-    synced = false
-    playing = false
-  }
-  clearInterval(scrubber)
-})
-
-io.socket.on('updateTime', function (data){
-  if (mobileReady){
-    if (myid != data.id){
-      if (isNaN(low.offset)){
-        low.offset = data.offset
-      }
-      else if (isNaN(data.offset)){
-        data.offset = low.offset
-      }
-      if (player.paused){
-        play()
-      }
-      var offset = parseFloat(low.offset) - parseFloat(data.offset)
-      var delay = parseFloat(((+new Date() - data.time + offset) / 1000).toFixed(6))
-      var time = data.currentTime + delay
-      var bottomCheck = -0.018, aboveCheck = 0.018
-      if (Math.abs(player.currentTime - time) > 0.035){
-        synced = false
-      }
-      if (synced){
-        bottomCheck = -0.030
-        aboveCheck = 0.030
-      }
-      else {
-        player.volume = 0
-        $('.syncingContainer').show()
-      }
-
-      if (player.currentTime - time < bottomCheck){
-        player.currentTime = parseFloat( time ) + parseFloat(learn.adjust('below'))
-        console.log('below')
-        player.volume = 0
-        $('.syncingContainer').show()
-      }
-      else if (player.currentTime - time > aboveCheck){
-        player.currentTime = parseFloat( time ) + parseFloat(learn.adjust('above'))
-        console.log('above')
-        player.volume = 0
-        $('.syncingContainer').show()
-      }
-      else {
-        synced = true
-        player.volume = 1
-        $('.syncingContainer').hide()
-      }
-    }
-  }
-})
-
-io.socket.on('addSong', function (data){
-  addSongToList(data)
-  if (!currentSong){
-    addSongToBottom(data)
-    setTimeout(function(){
-      loadSong(data.url)
-    }, 1000)
-  }
-})
-
-io.socket.on('changeSong', function (data){
-  console.log('change song')
-  changeSong(data)
-})
+//
+// io.socket.on('play', function (data){
+//   if (myid != data.id){
+//     var offset = parseFloat(low.offset) - parseFloat(data.offset)
+//     var delay = ((+new Date() - data.time + offset) / 1000).toFixed(6)
+//     if (delay < 0)
+//       delay = 0
+//
+//     player.currentTime = parseFloat( (player.currentTime).toFixed(6) + parseFloat(delay) )
+//     play()
+//   }
+// })
+//
+// io.socket.on('pause', function (data){
+//   $('#play').css('background-image', 'url(/images/play.jpg)')
+//   if (myid != data.id){
+//     player.pause()
+//     synced = false
+//     playing = false
+//   }
+//   clearInterval(scrubber)
+// })
+//
+// io.socket.on('updateTime', function (data){
+//   if (mobileReady){
+//     if (myid != data.id){
+//       if (isNaN(low.offset)){
+//         low.offset = data.offset
+//       }
+//       else if (isNaN(data.offset)){
+//         data.offset = low.offset
+//       }
+//       if (player.paused){
+//         play()
+//       }
+//       var offset = parseFloat(low.offset) - parseFloat(data.offset)
+//       var delay = parseFloat(((+new Date() - data.time + offset) / 1000).toFixed(6))
+//       var time = data.currentTime + delay
+//       var bottomCheck = -0.018, aboveCheck = 0.018
+//       if (Math.abs(player.currentTime - time) > 0.030){
+//         synced = false
+//       }
+//       if (synced){
+//         bottomCheck = -0.030
+//         aboveCheck = 0.030
+//       }
+//       else {
+//         player.volume = 0
+//         $('.syncingContainer').show()
+//       }
+//
+//       if (player.currentTime - time < bottomCheck){
+//         player.currentTime = parseFloat( time ) + parseFloat(learn.adjust('below'))
+//         player.volume = 0
+//         $('.syncingContainer').show()
+//       }
+//       else if (player.currentTime - time > aboveCheck){
+//         player.currentTime = parseFloat( time ) + parseFloat(learn.adjust('above'))
+//         player.volume = 0
+//         $('.syncingContainer').show()
+//       }
+//       else {
+//         synced = true
+//         player.volume = 1
+//         $('.syncingContainer').hide()
+//       }
+//     }
+//   }
+// })
+//
+// io.socket.on('addSong', function (data){
+//   addSongToList(data)
+//   if (!currentSong){
+//     addSongToBottom(data)
+//     setTimeout(function(){
+//       loadSong(data.url)
+//     }, 1000)
+//   }
+// })
+//
+// io.socket.on('changeSong', function (data){
+//   changeSong(data)
+// })
