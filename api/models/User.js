f84aacda173e2513a882b4f08370a0f3b4571a3d
WillBMease <will@jellyvibes.com>
2016-09-11 22:56:48 -0700

    d

--- api/models/User.js
+++ api/models/User.js
@@ -6,7 +6,7 @@ var User = {
   attributes: {
     username  : { type: 'string', unique: true },
     email     : { type: 'email',  unique: true },
-    passports : { collection: 'Passport', via: 'user' }
+    passports : { collection: 'Passport', via: 'user' },
 
     room: {
       model: 'room'
