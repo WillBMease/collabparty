6bfd553e872f5e050bd11477c6ec74fba13a412f
WillBMease <will@jellyvibes.com>
2016-09-12 23:14:00 -0700

    d

--- assets/js/sync.js
+++ assets/js/sync.js
@@ -5,23 +5,25 @@ var Sync = function(){
 }
 
 Sync.prototype.startPing = function(){
-  this.pingInterval = setInterval(this.ping, 45)
+  this.pingInterval = setInterval(this.ping, 1005)
 }
 
 Sync.prototype.ping = function(){
   io.socket.post('/Room/ping/', {start: +new Date()}, function (data){
     data.latency = (+new Date() - data.start) / 2
+    console.log(data)
+    console.log(this.low)
     if (data.latency < this.low.latency){
       this.low = data
     }
+    this.pingct++
+    if (this.pingct > 100){
+      clearInterval(this.pingInterval)
+      this.low.offset = this.low.server - this.low.start - this.low.latency
+      this.low.userid = userid
+      io.socket.post('/Room/storeOffset', this.low)
+    }
   })
-  this.pingct++
-  if (this.pingct > 100){
-    clearInterval(this.pingInterval)
-    this.low.offset = this.low.server - this.low.start - this.low.latency
-    this.low.userid = userid
-    io.socket.post('/Room/storeOffset', this.low)
-  }
 }
 
 sync = new Sync()
