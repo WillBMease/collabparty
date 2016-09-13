39bee01bc36c1da6c89ab771ad5774044684fbe4
WillBMease <will@jellyvibes.com>
2016-09-12 23:23:09 -0700

    d

--- assets/js/sync.js
+++ assets/js/sync.js
@@ -2,28 +2,50 @@ var Sync = function(){
   var low = {latency: 999999}
   var pingInterval
   var pingct = 0
+
+  var startPing = function(){
+    this.pingInterval = setInterval(this.ping, 1005)
+  }
+
+  var ping = function(){
+    io.socket.post('/Room/ping/', {start: +new Date()}, function (data){
+      data.latency = (+new Date() - data.start) / 2
+      console.log(data)
+      console.log(this.low)
+      if (data.latency < this.low.latency){
+        this.low = data
+      }
+      this.pingct++
+      if (this.pingct > 100){
+        clearInterval(this.pingInterval)
+        this.low.offset = this.low.server - this.low.start - this.low.latency
+        this.low.userid = userid
+        io.socket.post('/Room/storeOffset', this.low)
+      }
+    })
+  }
 }
 
-Sync.prototype.startPing = function(){
-  this.pingInterval = setInterval(this.ping, 1005)
-}
+// Sync.prototype.startPing = function(){
+//   this.pingInterval = setInterval(this.ping, 1005)
+// }
 
-Sync.prototype.ping = function(){
-  io.socket.post('/Room/ping/', {start: +new Date()}, function (data){
-    data.latency = (+new Date() - data.start) / 2
-    console.log(data)
-    console.log(low)
-    if (data.latency < low.latency){
-      low = data
-    }
-    this.pingct++
-    if (this.pingct > 100){
-      clearInterval(this.pingInterval)
-      low.offset = low.server - low.start - low.latency
-      low.userid = userid
-      io.socket.post('/Room/storeOffset', low)
-    }
-  })
-}
+// Sync.prototype.ping = function(){
+//   io.socket.post('/Room/ping/', {start: +new Date()}, function (data){
+//     data.latency = (+new Date() - data.start) / 2
+//     console.log(data)
+//     console.log(this.low)
+//     if (data.latency < this.low.latency){
+//       this.low = data
+//     }
+//     this.pingct++
+//     if (this.pingct > 100){
+//       clearInterval(this.pingInterval)
+//       this.low.offset = this.low.server - this.low.start - this.low.latency
+//       this.low.userid = userid
+//       io.socket.post('/Room/storeOffset', this.low)
+//     }
+//   })
+// }
 
 sync = new Sync()
