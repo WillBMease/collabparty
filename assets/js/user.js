21f74ac7a887b24e4c862fe768f6e8a399393a03
WillBMease <will@jellyvibes.com>
2016-09-13 20:56:38 -0700

    d

--- assets/js/user.js
+++ assets/js/user.js
@@ -1,7 +1,9 @@
-var User = function(){
-  var roomid = (window.location.hash).replace('#', '')
-  if (!document.cookie){
-  	document.cookie = 'username='+getRandomToken()+'; expires=Thu, 18 Dec 2020 12:00:00 UTC'
-  }
-  var id = document.cookie
-}
+// NOT CURRENTLY IN USE
+
+// var User = function(){
+//   var roomid = (window.location.hash).replace('#', '')
+//   if (!document.cookie){
+//   	document.cookie = 'username='+getRandomToken()+'; expires=Thu, 18 Dec 2020 12:00:00 UTC'
+//   }
+//   var id = document.cookie
+// }
