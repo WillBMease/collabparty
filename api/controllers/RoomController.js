24f7fb5e023da32c9edb4c78d0b2b8ad4ced6b51
William Bingham Mease <w@rageon.com>
2018-03-30 14:49:10 -0700

    push

--- api/controllers/RoomController.js
+++ api/controllers/RoomController.js
@@ -6,7 +6,8 @@
  */
 
 var fs = require('fs');
-var youtubedl = require('youtube-dl');
+// var youtubedl = require('youtube-dl');
+var youtubedl
 
 var alphabet = ['A','B','C','D','E','F','G','H','J','K','M','N','P','Q','R','S','T','U','V','W','X','Y','Z']
 var digits = [2,3,4,5,6,7,8,9]
